0d195a7c3deba3cf544b572ea30d6828acd33d02
Spandana Nannapaneni <spandana@thoughtworks.com>
2021-03-04 17:35:15 +0530

    Refactored tests

--- src/test/java/com/vapasi2021/helpers/Driver.java
+++ src/test/java/com/vapasi2021/helpers/Driver.java
@@ -1,38 +1,19 @@
 package com.vapasi2021.helpers;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.*;
 
 public class Driver {
     public static WebDriver driver;
-
-    public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "/Users/spandana/Downloads/chromedriver");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://spree-vapasi.herokuapp.com");
-        System.out.println(driver.getTitle());
-
-        driver.findElement(By.id("link-to-login")).click();
-        driver.findElement(By.id("spree_user_email")).sendKeys("spandana@thoughtworks.com");
-        driver.findElement(By.id("spree_user_password")).sendKeys("spd1245678");
-        driver.findElement(By.cssSelector("#new_spree_user > p:nth-child(5) > input")).click();
-        WebElement error = driver.findElement(By.cssSelector("#content > div.alert.alert-error"));
-        Assert.assertEquals(error.getText(), "Invalid email or password.");
-
-        driver.quit();
-    }
-
+    public static WebDriverWait wait;
 
     @BeforeMethod
     public void setDriver() {
         System.setProperty("webdriver.chrome.driver", "/Users/spandana/Downloads/chromedriver");
         driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
         driver.get("https://spree-vapasi.herokuapp.com");
     }
 
--- src/test/java/com/vapasi2021/tests/Tests.java
+++ src/test/java/com/vapasi2021/tests/Tests.java
@@ -2,35 +2,52 @@ package com.vapasi2021.tests;
 
 import com.vapasi2021.helpers.Driver;
 import org.openqa.selenium.By;
+
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+
 public class Tests extends Driver {
+
+    public String emailId;
+    public String password;
+
     @Test
     public void verifyInvalidLoginMessage() {
         driver.findElement(By.id("link-to-login")).click();
         driver.findElement(By.id("spree_user_email")).sendKeys("spandana@thoughtworks.com");
         driver.findElement(By.id("spree_user_password")).sendKeys("acdeek");
-        driver.findElement(By.cssSelector("#new_spree_user > p:nth-child(5) > input")).click();
-        String error = driver.findElement(By.cssSelector("#content > div.alert.alert-error")).getText();
+        driver.findElement(By.cssSelector("input[name ='commit']")).isDisplayed();
+        driver.findElement(By.cssSelector("input[name ='commit']")).click();
+        String error = driver.findElement(By.cssSelector("div[class='alert alert-error']")).getText();
         Assert.assertEquals(error, "Invalid email or password.");
-    }
-
-    @Test
-    public void verifyLoginSuccessMessage() {
-        driver.findElement(By.id("link-to-login")).click();
-        driver.findElement(By.id("spree_user_email")).sendKeys("spandana@thoughtworks.com");
-        driver.findElement(By.id("spree_user_password")).sendKeys("spd12345678");
-        driver.findElement(By.cssSelector("#new_spree_user > p:nth-child(5) > input")).click();
-        String successMessage = driver.findElement(By.cssSelector("#content > div.alert.alert-success")).getText();
-        Assert.assertEquals(successMessage, "Logged in successfully");
     }
 
     @Test
     public void verifySearch() {
         driver.findElement(By.id("keywords")).sendKeys("Baseball");
-        driver.findElement(By.cssSelector("#search-bar > form > input.btn.btn-success"));
-        driver.findElement(By.cssSelector("#product_6 > div > div.panel-body.text-center.product-body > a")).isDisplayed();
+        driver.findElement(By.cssSelector("input[class='btn btn-success']"));
+        driver.findElement(By.linkText("Ruby on Rails Baseball Jersey")).isDisplayed();
 
+    }
+
+
+    public void login(String emailId, String password) {
+        driver.findElement(By.id("link-to-login")).click();
+        driver.findElement(By.id("spree_user_email")).sendKeys(emailId);
+        driver.findElement(By.id("spree_user_password")).sendKeys(password);
+        driver.findElement(By.cssSelector("input[name ='commit']")).click();
+    }
+
+    @Test
+    public void verifyLoginSuccessMessage() {
+        //arrange
+        emailId = "spandana@thoughtworks.com";
+        password = "spd12345678";
+        //act
+        login(emailId, password);
+        String successMessage = driver.findElement(By.cssSelector("div[class='alert alert-success']")).getText();
+        //assert
+        Assert.assertEquals(successMessage, "Logged in successfully");
     }
 }
